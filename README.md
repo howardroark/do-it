db3d869f55657b55806f9762a9885f494be82e39
Howard Roark <howie@codingcoop.org>
2016-05-18 09:39:00 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# DoButton
+
+A "Deploy to Heroku" style button for DigitalOcean.
+
+Try it out...
+
+[![](https://raw.githubusercontent.com/howardroark/dobutton/develop/DO.png)](https://dobutton.surge.sh/howardroark/meteorops)
+
+This project is heavily inspired by [Do Install Button](https://github.com/seven1m/do-install-button) :)
+
+### Development
+
+```
+$ npm install
+$ npm start
+```
---
+++ scripts.js
@@ -0,0 +1,127 @@
+var parser = document.createElement('a')
+parser.href = document.location.href
+var path = parser.pathname
+var baseURL = parser.protocol+"//"+parser.host
+
+var clientId
+if(parser.hostname == "localhost") {
+    clientId = "6e3296a6eaa870f520ee50a2f6704d7bcfff3d50db52141f5685424993deb012"
+} else {
+    clientId = "1235c3cc108113ba9a3c1103453ac1f0fe0f436e8e203653d13abad391be6097"
+}
+
+if(path == "/") {
+    //
+}
+
+if(path == "/callback") {
+    var callback = parseQuery(window.location.hash)
+    if(typeof callback.error == 'undefined') {
+        Cookies.set( 'AccessToken', callback.access_token )
+    } else {
+
+    }
+    window.close()
+}
+
+var username = path.split('/')[1]
+var project = path.split('/')[2]
+
+var state = {}
+
+var url = 'https://api.github.com/repos/'+username+'/'+project
+
+if(typeof Cookies.get('GitHubToken') != 'undefined') {
+    url = url+'?access_token='+Cookies.get('GitHubToken')
+}
+
+$.ajax({
+    async: false,
+    url: url,
+    statusCode: {
+        403: function() {
+            var github_token = prompt("The GitHub API has been called too many times. You can enter a personal access token to have unlimited access.")
+            if(github_token != null) {
+                Cookies.set('GitHubToken', github_token)
+            }
+        }
+    },
+    success: function(data) {
+        state.repo = data
+    }
+})
+
+$.ajax({
+    async: false,
+    url: 'https://raw.githubusercontent.com/'+username+'/'+project+'/'+state.repo.default_branch+'/project.json',
+    success: function(data) {
+        state.project = JSON.parse(data)
+    }
+})
+
+nunjucks.configure({ autoescape: true })
+
+var projectTemplate = $('script[name=project]').text()
+
+var projectHTML = nunjucks.renderString(projectTemplate, state)
+
+$('body').append(projectHTML)
+
+$('button').click(function(e) {
+    if(typeof Cookies.get('AccessToken') == 'undefined') {
+        window.open("https://cloud.digitalocean.com/v1/oauth/authorize?response_type=token&client_id="+clientId+"&redirect_uri="+baseURL+"/callback&scope=read+write",
+                    "oauth",
+                    "menubar=1,resizable=1,width=860,height=600")
+    } else {
+        $.ajax({
+            async: false,
+            url: 'https://api.digitalocean.com/v2/account/keys',
+            beforeSend: function(xhr){xhr.setRequestHeader('Authorization', 'Bearer '+Cookies.get('AccessToken'))},
+            success: function(data) {
+                state.ssh_keys = data.ssh_keys
+            }
+        })
+        if(state.ssh_keys.length == 0) {
+            alert("Your DigitalOcean account must have an active ssh-key.")
+        } else {
+            var formData = $('form').serializeObject()
+            var keys = []
+            for(var i = 0; i < state.ssh_keys.length; i++) {
+                keys[i] = state.ssh_keys[i].id
+            }
+            var cloudConfig = "#cloud-config\n"+YAML.stringify(state.project.config)
+            var dropletRequestData = {
+                name: formData.name,
+                region: formData.region,
+                size: formData.size,
+                image: formData.image,
+                ssh_keys: keys,
+                user_data: cloudConfig 
+            }
+            $.ajax({
+                async: false,
+                type: 'POST',
+                url: 'https://api.digitalocean.com/v2/droplets',
+                data: JSON.stringify(dropletRequestData),
+                contentType: 'application/json',
+                beforeSend: function(xhr){xhr.setRequestHeader('Authorization', 'Bearer '+Cookies.get('AccessToken'))},
+                success: function(data) {
+                    state.droplet = data.droplet
+                }
+            })
+            console.log(state, cloudConfig)
+        }
+    }
+
+    return false
+})
+
+function parseQuery(qstr) {
+    var query = {};
+    var a = qstr.substr(1).split('&');
+    for (var i = 0; i < a.length; i++) {
+        var b = a[i].split('=');
+        query[decodeURIComponent(b[0])] = decodeURIComponent(b[1] || '');
+    }
+    return query;
+}
