899c5b1365dfdf5e26aa94cb5182c7f9fbdd1731
Howard Roark <howie@codingcoop.org>
2016-06-15 08:53:12 -0700

    Updating copy.

--- script.js
+++ script.js
@@ -138,7 +138,7 @@ $.ajax({
                                                                         console.log(data)
                                                                         if(data.status == 'complete') {
                                                                             clearInterval(scriptStatusInterval)
-                                                                            $('button').text("done! goto your project...")
+                                                                            $('button').text("GO!")
                                                                             $('button').click(function() {window.location.href = 'http://'+state.droplet.networks.v4[0].ip_address; return false})
                                                                             $('button').prop("disabled",false)
                                                                         }
