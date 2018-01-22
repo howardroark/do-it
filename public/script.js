0970bdecc4bcea8b6e46bf685661a7253753a52b
howardroark <howardroark@users.noreply.github.com>
2018-01-22 03:21:43 -0500

    Update script.js

--- public/script.js
+++ public/script.js
@@ -183,18 +183,17 @@ function parseQuery(qstr) {
 }
 
 function renderInstructions() {
-    $('button').prop("disabled",false);
-    $('button').text("GO!");
-    $('button').click(function() {var win = window.open('http://'+state.droplet.networks.v4[0].ip_address, '_blank'); win.focus(); return false});
-    $('fieldset').hide();
-    if(typeof state.project.instructions != 'undefined') {
-        $.ajax({
-            url: 'https://raw.githubusercontent.com/'+state.userName+'/'+state.projectName+'/'+state.project.branch+'/'+state.project.instructions,
-            success: function(data) {
-                $('.instructions').html(nunjucks.renderString(md.render(data), state));
-            }
-        });
-    }
+    var instructionsTemplate = $('script[name=instructions]').text();
+
+    $.ajax({
+        url: 'https://raw.githubusercontent.com/'+state.userName+'/'+state.projectName+'/'+state.project.branch+'/'+state.project.instructions,
+        success: function(data) {
+            var instructionsHTML = nunjucks.renderString(instructionsTemplate, {
+                markdown: nunjucks.renderString(md.render(data), state)
+            });
+            $('body').html(instructionsHTML);
+        }
+    });
 }
 
 function doProject() {
